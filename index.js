3c75986798009a642486eb30feda3da41627c5c0
Azim-Ul-Karim <azimulkarim.ruet@gmail.com>
2025-12-18 20:13:15 +0600

    Updated

--- index.js
+++ index.js
@@ -1,19 +1,56 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const crypto = require('crypto');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 const port = process.env.PORT || 3000;
+
+const admin = require("firebase-admin");
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+function generateTrackingId() {
+  const prefix = 'ODR';
+  const date = new Date().toISOString().slice(0, 10).replace(/-/g, "");
+  const random = crypto.randomBytes(3).toString("hex").toUpperCase();
+  return `${prefix}-${date}-${random}`;
+}
 
 app.use(express.json());
 app.use(cors());
 
+const verifyFBToken = async (req, res, next) => {
+  const token = req.headers?.authorization;
+
+  if (!token) {
+    return res.status(401).send({ message: 'Unauthorized Access.' });
+  }
+
+  try {
+    const idToken = token.split(' ')[1];
+    const decoded = await admin.auth().verifyIdToken(idToken);
+    req.decoded_email = decoded.email;
+    next();
+  }
+  catch (err) {
+    return res.status(401).send({ message: 'Unauthorized Access.' })
+  }
+
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0tgm6si.mongodb.net/?appName=Cluster0`;
 
 app.get('/', (req, res) => {
-
-})
+  res.send("Chefonex in operation...");
+});
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -25,14 +62,56 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded_email;
+      const user = await usersCollection.findOne({ email });
 
+      if (!user || user.role !== 'admin') {
+        return res.status(403).send({ message: 'Forbidden Access' });
+      }
+      next();
+    };
 
+    const db = client.db('Chefonex_DB');
 
+    // Users APIs
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const usersCollection = db.collection('users');
+
+    app.get('/users', verifyFBToken, async (req, res) => {
+      const cursor = usersCollection.find().sort({ createdAt: -1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const email = user.email;
+      const userExists = await usersCollection.findOne({ email });
+
+      if (userExists) {
+        return res.send({ message: 'User Exists' });
+      }
+
+      const newUser = {
+        displayName: user.displayName,
+        email: user.email,
+        photoURL: user.photoURL,
+        address: user.address,
+        role: 'user',
+        status: 'active',
+        chefId: null,
+        createdAt: new Date()
+      };
+
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    
+
   } finally {
     // await client.close();
   }
